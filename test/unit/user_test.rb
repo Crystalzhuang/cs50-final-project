5308f370f0068c4e406202a298fd3977a0b8e42d
Tian Zhuang <crystalzhuang1996@gmail.com>
2013-11-09 20:02:29 -0500

    Added tests for user first/last name

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "users should enter first name" do 
+	 	user = User.new
+	 	assert !user.save
+	 	assert !user.errors[:first_name].empty?
+	end
+
+	test "users should enter last name" do 
+	 	user = User.new
+	 	assert !user.save
+	 	assert !user.errors[:last_name].empty?
+	 end
+
+	test "users should enter profile name" do 
+	 	user = User.new
+	 	assert !user.save
+	 	assert !user.errors[:profile_name].empty?
+	end
+
+	test "users should enter profile name" do	
+		user = User.new
+	 	users.profile_name = users(:Crystal).profile_name
+	 	
+	 	assert !user.save
+	 	assert !user.errors[:profile_name].empty?
+	end
 end
